6669c16c89e2b0934b943b486f6720a771b00ce2
James Smith <james@smithjw.me>
2018-09-05 16:15:09 +1000

    It's all working now 🙌 Need to update my serverless file to trigger this script off SNS too

--- update_db.py
+++ update_db.py
@@ -2,6 +2,7 @@ import os
 import json
 import boto3
 import re
+from botocore.exceptions import ClientError
 
 
 def extract_user_id(text):
@@ -9,40 +10,55 @@ def extract_user_id(text):
 
     return user_id
 
+def create_user_item(table, user_id, timestamp):
+    try:
+        table.put_item(
+            #Key = {'user_id': user_id},
+            Item = {'user_id': user_id, 'biebered_by': {}},
+            #UpdateExpression = 'SET biebered_by = {}',
+            ConditionExpression = 'attribute_not_exists(user_id)'
+        )
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == 'ConditionalCheckFailedException':
+            print(f'User already exists, updating ID: {user_id}')
+        else:
+            print(f'Unexpected error: {error_code}')
 
 def write_db_data(message, timestamp):
     table_name = os.environ['biebered_table_name']
-    #dynamo = boto3.client('dynamodb', region_name='us-east-1')
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table(table_name)
 
     user_id = message['user_id'][0]
-    # biebered_by = message['text'][0]
-    biebered_by = extract_user_id(message['text'][0])
+    biebered_by_id = extract_user_id(message['text'][0])
 
-    print(user_id, biebered_by, timestamp)
+    #print(user_id, biebered_by_id, timestamp)
 
+    print(f'Creating Item with ID: {user_id} if it doesn\'t already exist')
+    create_user_item(table, user_id, timestamp)
 
     response = table.update_item(
-        Key={
-            'user_id': user_id
+        Key={'user_id': user_id},
+        UpdateExpression = 'SET #bb.#ts = :update',
+        ExpressionAttributeNames = {
+            '#bb': 'biebered_by',
+            '#ts': timestamp
         },
-        UpdateExpression='SET biebered_by = :vals',
-        ExpressionAttributeValues={
-            ':vals': {
-                timestamp: {
-                    'user_id': biebered_by,
-                    'test': 'Blah'
-                }
+        ExpressionAttributeValues = {
+            ":update": {
+                'user_id': biebered_by_id,
+                # Call out to Slack API to grab biebered_by User information
+                'first_name': 'first_name'
             }
         }
     )
 
     return response
 
+
 def main(event, timestamp):
     test_message = json.loads(event)
-    print(test_message)
 
     write_db_data(test_message, timestamp)
 
@@ -56,6 +72,6 @@ def lambda_func(event, context):
 if __name__ == '__main__':
     with open('test_message.json', encoding='utf-8') as json_file:
         message = json_file.read()
-    timestamp = '2018-09-06T04:13:39.960Z'
+    timestamp = '2018-09-11T04:13:39.960Z'
 
     main(message, timestamp)
